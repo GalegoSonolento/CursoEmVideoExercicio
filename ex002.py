80c71187dc6197318e2d66b8241b7c00ddbf9cb4
HenryH <henriquehaag11@gmail.com>
2022-03-05 18:23:19 -0300

    commit

---
+++ ex002.py
@@ -0,0 +1,5 @@
+# Desafio 2: Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas vindas
+
+nome = input('Digite seu nome aqui por favor: ')
+
+print('Olá, {}! Prazer em conhecê-lo.'.format(nome))
