a460726353cfa0cd2923b9ac94a85b51528efdf0
HenryH <henriquehaag11@gmail.com>
2022-03-27 20:04:55 -0300

    commit

--- Desafio/des026.py
+++ Desafio/des026.py
@@ -2,4 +2,8 @@
 # Quantas vezes aparece a letra A
 # Em que posição ela aparece a primeira vez
 # Em que posição ela aparece a última vez
-
+frase = str(input('Digite uma pequena frase: ')).strip().lower()
+print('Na sua frase, a letra \'a\' aparece {} vezes'.format(frase.count('a')))
+print('A letra \'a\' aparece pela primeira vez na posição {}'.format(frase.find('a')))
+ultimo = frase.rfind('a')
+print(ultimo)
--- Desafio/des027.py
+++ Desafio/des027.py
@@ -2,3 +2,10 @@
 # EX: Ana Maria de Souza
 # primeiro = Ana
 # último = Souza
+nome = str(input('Digite seu nome completo: ')).lower().strip()
+primeiro = nome.split()
+print(primeiro[0].capitalize())
+count = -1
+for i in primeiro:
+    count += 1
+print(primeiro[count].capitalize())
