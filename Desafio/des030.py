992766ecbbe330925f0dce3474d27110af55b587
HenryH <henriquehaag11@gmail.com>
2022-03-28 17:34:18 -0300

    commit

---
+++ Desafio/des030.py
@@ -0,0 +1,1 @@
+# Crie um programa que leia um número inteiro qualquer e mostre na tela se é par ou ímpar
---
+++ Desafio/des031.py
@@ -0,0 +1,2 @@
+# Desenvolva um programa qu pergunta a distância de uma viagem em
+# calcule o preço da passagem, cobrando R$0,50 por km para viagens até 200km e R$0,40 para viagens mais longas
