37331fe60da2dd29d766f255430dbfb3dbc3d47a
HenryH <henriquehaag11@gmail.com>
2022-03-31 10:30:16 -0300

    commit

---
+++ Desafio/desEXTRA.py
@@ -0,0 +1,4 @@
+# Pegar todos os desafios anteriores e colocar um código de cores dentro deles
+# Não é pra refazer o exercício todo, mas só colocar os códigos de cor dentro deles
+# easy = pelo menos 10
+# hard = fazer todos os 35
