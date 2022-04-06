409a883ef57e4eab5ce90d64ecf8587d979a335c
HenryH <henriquehaag11@gmail.com>
2022-04-06 15:26:16 -0300

    commit

--- Desafio/des36.py
+++ Desafio/des36.py
@@ -1,3 +1,13 @@
 # Escreva um programa para aprovar o empréstimo bancário para a compra de uma casa. O programa vai perguntar o valo
 # da casa, o salário do comprador, e em quantos anos ele vai pagar.
 # Calcule o valor da prestação mensal, sabendo que ela não pode exceder 30% do salário ou então o empréstimo será negado
+print('Esse programa validará ou não a compra da casa. Pro favor, responda as perguntas abaixo: ')
+valorCasa = float(input('Digite o valor do imóvel: '))
+salarioCompr = float(input('Digite seu salário: '))
+prest = float(input('Digite em quantos anos a casa será paga: '))
+meses = prest * 12
+prest = valorCasa / meses
+if prest < (salarioCompr * 0.3):
+    print('Seu empréstimo foi aprovado')
+else:
+    print('Seu empréstimo foi reprovado')
--- Desafio/des37.py
+++ Desafio/des37.py
@@ -2,3 +2,14 @@
 #1 - binário
 #2 - octal
 #3 - hexadecimal
+num = int(input('Digite um número para a máquina: '))
+print('Qual conversão deseja fazer? '
+      '\n [1] converter para BINÁRIO'
+      '\n converter para OCTAL'
+      '\n converter para HEXADECIMAL')
+escolha =
+binary = format(int(input('Digite um valor inteiro: ')), 'b')
+print(binary)
+octal = "{0:o}".format(int(input('Digite outro valor inteiro: ')))
+print(octal)
+print(format(int(input("Digite um número float: ")), 'x'))
