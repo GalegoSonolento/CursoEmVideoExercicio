118cd0c3cab3d75e50391c691a4851119f04f266
HenryH <henriquehaag11@gmail.com>
2022-04-09 22:17:23 -0300

    commit

--- Desafio/des44.py
+++ Desafio/des44.py
@@ -4,3 +4,22 @@
 #- à vista no cartão: 5% de desconto
 #- em até 2x no cartão: preço normal
 #- 3x ou mais no cartão: 20% de juros
+preco = 100
+print('Você deliberadamente vai comprar um tatu assado por 100 mangos '
+      '\n Por favor digite a forma de pagamento abaixo:'
+      '\n 1 - À vista em dinheiro/ cheque (10% de desconto)'
+      '\n 2 - À vista no cartão (5% de desconto)'
+      '\n 3 - Em até 2x no cartão (preço normal)'
+      '\n 4 - 3x ou mais no cartão (20% de juros)')
+opcao = int(input('Digite sua opção: '))
+if opcao == 1:
+      precoN = preco - (preco * 0.1)
+      print('Pagarás {}'.format(precoN))
+elif opcao == 2:
+      precoN = preco - (preco * 0.05)
+      print('pagrás R${}'.format(precoN))
+elif opcao == 3:
+      print('Pagarás R${}'.format(preco))
+elif opcao == 4:
+      precoN = preco + (preco * 0.2)
+      print('Pagarás R${}'.format(precoN))
--- Desafio/des45.py
+++ Desafio/des45.py
@@ -1,1 +1,37 @@
 # Crie um programa que faça o computador jogar jokenpô com você (pedra, papel, tesoura)
+import random
+import time
+jokenpo = random.choice(['Pedra', 'Papel', 'Tesoura'])
+print('-=' * 12)
+print('\033[31m JOGUE JOKENPÔ COM A MÁQUINA!\033[m')
+print('-=' * 12)
+print('\033[32m ESOLHA UMA DENTRE AS OPÇÕES ABAIXO:'
+      '\n -1- Pedra'
+      '\n -2- Papel'
+      '\n -3- Tesoura\033[m')
+opcao = int(input('Digite aqui: '))
+print('Processando...')
+time.sleep(3)
+print('\033[1;35m-_'*12)
+if opcao == 1:
+    if jokenpo == 'Pedra':
+        print('\033[1;35m VOCÊ EMPATOU COM A MÁQUINA!')
+    elif jokenpo == 'Papel':
+        print('\033[1;35mVOCÊ PERDEU PARA A MÁQUINA!')
+    elif jokenpo == 'Tesoura':
+        print('\033[1;35mVOCÊ GANHOU DA MÁQUINA!')
+elif opcao == 2:
+    if jokenpo == 'Pedra':
+        print('\033[1;35mVOCÊ VENCEU A MÁQUINA!')
+    elif jokenpo == 'Papel':
+        print('\033[1;35mVOCÊ EMPATOU COM A MÁQUINA!')
+    elif jokenpo == 'Tesoura':
+        print('\033[1;35mVOCÊ PERDEU PARA A MÁQUINA!')
+elif opcao == 3:
+    if jokenpo == 'Pedra':
+        print('\033[1;35mVOCÊ PERDEU PARA A MÁQUINA!')
+    elif jokenpo == 'Papel':
+        print('\033[1;35mVOCÊ GANHOU DA MÁQUINA!')
+    elif jokenpo == 'Tesoura':
+        print('\033[1;35mVOCÊ EMPATOU COM A MÁQUINA!')
+print('-_'*12)
