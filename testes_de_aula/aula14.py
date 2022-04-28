3cc1ba96da19f358871eb6851f9341c175be9dcb
HenryH <henriquehaag11@gmail.com>
2022-04-28 10:32:23 -0300

    commit

---
+++ testes_de_aula/aula14.py
@@ -0,0 +1,16 @@
+# c = 1
+# while c < 10:
+#     print(c)
+#     c += 1
+# print('fim')
+
+# r = 'S'
+# while r == 'S':
+#     n = int(input('Digite um valor: '))
+#     r = str(input('Quer continuar? [S/N] ')).upper()
+# print( 'fim')
+
+n = 1
+while n != 0:
+    n = int(input('Digite um valor: '))
+print('Acabou')
