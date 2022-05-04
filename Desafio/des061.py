3ca23bdc3a1554c86b88badd70e9cf42b4d92985
HenryH <henriquehaag11@gmail.com>
2022-05-04 10:29:00 -0300

    commit

--- Desafio/des061.py
+++ Desafio/des061.py
@@ -1,2 +1,22 @@
 # Refaça o desafio 51, lendo o primeiro termo e a razaõ de uma PA, mostrando os 10 primeiros termos da rpogressão
-#usando a estrutura while
+# usando a estrutura while
+
+# # Desenvolva um programa que leia o primeiro termo e a razão de uma PA. No final, mostre os 10 primeiros termos dessa
+# # progressão
+# a1 = int(input('Diga o primeiro termo de uma PA: '))
+# r = int(input('Diga a razão da PA: '))
+# if a1 == r:
+#     for i in range(a1, r * 11, r):
+#         print(i)
+# else:
+#     for j in range(a1, r * 10, r):
+#         print(j)
+
+# an = a1 + (n-1)r
+a1 = int(input('Digite o primeiro temo da PA: '))
+r = int(input('Digite a razão dessa PA: '))
+cont = 1
+termos = 10  # If it where without a prefixed number, I would ask the terms to the user
+while cont <= termos:
+    print('{}° termo: {}'.format(cont, a1 + (cont - 1) * r))
+    cont += 1
--- Desafio/des062.py
+++ Desafio/des062.py
@@ -1,2 +1,18 @@
 # Melhore o desafio 61, perguntando para o usuário se elçe quer mostrar mais alguns termos. O program encerra
 # quando ele disser que quer mostrar 0 termos
+a1 = int(input('Digite o primeiro temo da PA: '))
+r = int(input('Digite a razão dessa PA: '))
+termos = 10
+cont = 1
+while cont <= termos:
+    print('{}° termo: {}'.format(cont, a1 + (cont - 1) * r))
+    cont += 1
+maisTermos = True
+while maisTermos:
+    mais = int(input('Digite quantos mais termos você deseja visualizar: '))
+    if mais <= 0:
+        print('Não se mostra mais zero ou de trás para frente!')
+        maisTermos = False
+    else:
+        print('{}° termo: {}'.format(cont, a1 + (cont - 1) * r))
+        cont += 1
