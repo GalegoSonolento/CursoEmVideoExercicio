84bc24affadff2bc003ae54d051bd57ee4a1238e
HenryH <henriquehaag11@gmail.com>
2022-05-13 13:35:29 -0300

    commit

--- Desafio/des069.py
+++ Desafio/des069.py
@@ -4,3 +4,28 @@
 # b) Quants homens foram cadastrados
 # c) Quantas mulheres tem menos de 20 anos
 # É necessário garantir que o programa funcionará para qualquer resposta
+
+from time import sleep
+quantM18 = manos = minasUnder20 = 0
+while True:
+    while True:
+        idade = int(input('Digite a idade da pessoa: '))
+        if idade > 0:
+            break
+    while True:
+        sexo = str(input('Digite o sexo [F/M]: ')).upper().strip()[0]
+        if sexo in 'FM':
+            break
+    if idade < 18:
+        quantM18 += 1
+    if sexo == 'M':
+        manos += 1
+    if sexo == 'F' and idade < 20:
+        minasUnder20 += 1
+    opcao = str(input('Deseja continuar [S/N]? ')).upper().strip()[0]
+    if opcao == 'N':
+        break
+    elif opcao == 'S':
+        print('Continuando...')
+        sleep(1)
+print(f'Foram cadastrados {quantM18} menores de 18 anos, {manos} homens e {minasUnder20}')
