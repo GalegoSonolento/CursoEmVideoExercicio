d3367f010dc0505787ae7fe29ac452b3ae23e1fe
HenryH <henriquehaag11@gmail.com>
2022-05-25 10:14:07 -0300

    commit

--- Desafio/des73.py
+++ Desafio/des73.py
@@ -3,3 +3,21 @@
 # b) os 4 últimos colocados.
 # c) Uma lista com os times em ordem alfabética.
 # d) Em que posição na tabela está o time da Chapecoense.
+
+brasileirao = ('Corinthians', 'Palmeiras', 'São Paulo', 'Atlético Mineiro', 'Botafogo', 'Santos', 'Fluminense',
+               'Coritiba', 'América Mineiro', 'Avaí', 'Internacional', 'Athletico-PR', 'Bragantino',
+               'Flamengo', 'Goiás', 'Cuiabá', 'Atlético Goianiense', 'Juventude', 'Ceará', 'Fortaleza')
+print('Cinco primeiros colocados: ')
+for i in range(len(brasileirao)):
+    if i < 5:
+        print(brasileirao[i])
+print(' ')
+print('Quatro últimos colocados:')
+for i in range(len(brasileirao)):
+    if i > 15:
+        print(brasileirao[i])
+print(' ')
+print('Tabela em ordem alfabética')
+tabelado = sorted(brasileirao)
+for i in range(len(tabelado)):
+    print(f'- {tabelado[i]}')
--- Exercícios gerais/ex072.py
+++ Exercícios gerais/ex072.py
@@ -10,4 +10,23 @@
 #     if i == numTec:
 #         print(f'Você escolheu o número {contagem[i]}')
 
-
+cont = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco',
+        'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze',
+        'treze', 'catorze', 'quinze', 'dezesseis', 'dezessete',
+        'dezoito', 'dezenove', 'vinte')
+while True:
+    while True:
+        num = int(input('Digite um número entre 0 e 20: '))
+        if 0 <= num <= 20:
+            break
+        print('Tente novamente', end=' ')
+    print(f'Você digitou o número {cont[num]}')
+    while True:
+        opcao = str(input('Deseja pergunar novamente? [S/N]')).strip().upper()[0]
+        if opcao not in 'SN':
+            print('Opção inválida.', end=' ')
+        else:
+            break;
+    if opcao == 'N':
+        break
+print('Obrigado por utilizar o programa')
