e6d2de1bf6ccdcf53e5a52705de9aedea16460bb
HenryH <henriquehaag11@gmail.com>
2022-09-19 18:25:19 -0300

    Exercício100-SoCorrigir

--- Desafio/des100.py
+++ Desafio/des100.py
@@ -3,33 +3,28 @@
 # A primeira função vai sortear 5 números e vai colocá-los dentro da lista
 # e a segunda função vai mostrar a soma entre todos os valores pares sorteados pela função anterior.
 
-import random
+from random import randint
 
 
 def sorteia():
     print('Sorteando 5 valores da lista: ', end='')
     vetor = []
     for i in range(0, 5):
-        vetor.append(i)
-        vetor[i] = random.randint
+        vetor.append(randint(1, 10))
         print(f'{vetor[i]} ', end='')
     print('PRONTO!')
-    print()
     return vetor
 
 
 def somaPar(vetor):
-    print(vetor)
-    # somaPares = 0
-    # if type(vetor) == list:
-    #     for i in range(0, len(vetor)):
-    #         if vetor[i] % 2 == 0:
-    #             somaPares += 1
-    # print(f'Somando os valores pares de {vetor}, temos {somaPares}')
+    somaPares = 0
+    if type(vetor) == list:
+        for i in range(0, len(vetor)):
+            if vetor[i] % 2 == 0:
+                somaPares += vetor[i]
+    print(f'Somando os valores pares de {vetor}, temos {somaPares}')
 
 
 # Código principal
-# somaPar(sorteia())
-sus = random.random
-# soma = sus+1
-print(sus)
+somaPar(sorteia())
+
---
+++ Exercícios gerais/ex100.py
@@ -0,0 +1,67 @@
+# # Exercício Python 100: Faça um programa que tenha uma lista chamada números e duas funções chamadas
+# # sorteia() e somaPar().
+# # A primeira função vai sortear 5 números e vai colocá-los dentro da lista
+# # e a segunda função vai mostrar a soma entre todos os valores pares sorteados pela função anterior.
+#
+# from random import randint
+#
+#
+# def sorteia():
+#     print('Sorteando 5 valores da lista: ', end='')
+#     vetor = []
+#     for i in range(0, 5):
+#         vetor.append(randint(1, 10))
+#         print(f'{vetor[i]} ', end='')
+#     print('PRONTO!')
+#     return vetor
+#
+#
+# def somaPar(vetor):
+#     somaPares = 0
+#     if type(vetor) == list:
+#         for i in range(0, len(vetor)):
+#             if vetor[i] % 2 == 0:
+#                 somaPares += vetor[i]
+#     print(f'Somando os valores pares de {vetor}, temos {somaPares}')
+#
+#
+# # Código principal
+# somaPar(sorteia())
+#
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
