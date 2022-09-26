e4b54402977aa43b23b9cc98147e412055c2d418
Henrique Haag <96831480+GalegoSonolento@users.noreply.github.com>
2022-09-26 17:05:43 -0300

    Update aula21.py

--- testes_de_aula/aula21.py
+++ testes_de_aula/aula21.py
@@ -50,15 +50,36 @@
 # print(f'No programa principal, x vale {x}')
 
 
-def funcao():
-    n1 = 4
-    print(f'n1 dentro vale {n1}')
+# def funcao():
+#    n1 = 4
+#    print(f'n1 dentro vale {n1}')
+#
+#
+# n1 = 2
+# print(f'n1 global vale {n1}')
+# funcao()
 
 
-n1 = 2
-print(f'n1 global vale {n1}')
-funcao()
+# def somar(a=0, b=0, c=0):
+#   s = a + b + c
+#   return s  # Função com retorno tem mais praticidade na hora de programar
 
+
+# r1 = somar(2, 3, 6)
+# r2 = somar(2, 2)
+# r3 = somar(6)
+# print(f'Os resultados foram {r1}, {r2} e {r3}')
+
+
+def fatorial(num=0):
+  f=1
+  for i in range(num, 0, -1):
+    f*=i
+  return f
+
+
+n = int(input("Digite um número: "))
+print(f'O fatorial de {n} é {fatorial(n)}')  # Funções com return precisam ser expressas
 
 
 
