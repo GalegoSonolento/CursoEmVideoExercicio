61fb44327a4a572fb79e473526c3a59818df0077
Henrique Haag <96831480+GalegoSonolento@users.noreply.github.com>
2022-10-03 09:26:13 -0300

    Update des102.py

--- Desafio/des102.py
+++ Desafio/des102.py
@@ -2,7 +2,20 @@
 # o primeiro que indique o número a calcular e outro chamado show,
 # que será um valor lógico (opcional) indicando se será mostrado ou não na tela o processo de cálculo do fatorial.
 
-def fatorial(num, show=False):
-    sus = 12
+def fatorial(ent, show=False):
+  print('-' * 30)
+  respShow = ''
+  num = ent
+  for i in range(num - 1, 0, -1):
+    if show and i == num - 1:
+      respShow += f'{num} * {i} '
+    elif show:
+      respShow += f'* {i} '
+      if i == 1:
+        respShow += '= '
+    num *= i
+  return respShow + f'{num}'
 
+  
 # Programa principal
+print(fatorial(5, show=True))
