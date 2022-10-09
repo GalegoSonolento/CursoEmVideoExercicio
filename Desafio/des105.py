7b4d682e0e265e6ef46762469f19017b8f315c30
HenryH <henriquehaag11@gmail.com>
2022-10-09 20:31:17 -0300

    Exercicio105

---
+++ Desafio/des105.py
@@ -0,0 +1,35 @@
+# Exercício Python 105: Faça um programa que tenha uma função notas() que pode receber várias notas de alunos e
+# vai retornar um dicionário com as seguintes informações:
+#
+# – Quantidade de notas
+# – A maior nota
+# – A menor nota
+# – A média da turma
+# – A situação (opcional)
+
+
+def notas(*n, sit=False):
+    menor_nota = maior_nota = soma = 0
+    for i, j in enumerate(n):
+        if i == 0:
+            menor_nota = maior_nota = j
+        if menor_nota > j:
+            menor_nota = j
+        if maior_nota < j:
+            maior_nota = j
+        soma += j
+    media = soma / len(n)
+    retorno = f'Total: {len(n)}, maior: {maior_nota}, menor: {menor_nota}, media: {media}'
+    situacao = 'RUIM'
+    if sit:
+        if media >= 7:
+            situacao = 'BOA'
+        elif 5 <= media < 7:
+            situacao = 'RAZOÁVEL'
+    retorno += f', situação: {situacao}'
+    return retorno
+
+
+#Programa principal
+resp = notas(2, 3, 10, sit=True)
+print(resp)
