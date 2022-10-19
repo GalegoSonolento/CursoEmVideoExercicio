ce4f792427638f26c68eff551a4fae121414adee
haaghenrique <henriquehaag11@gmail.com>
2022-10-19 10:26:40 -0300

    moedas

---
+++ Exercícios gerais/ex108/moeda.py
@@ -0,0 +1,22 @@
+def aumentar(preco=0, taxa=0):
+    res = preco + (preco * taxa / 100)
+    return moeda(res)
+
+
+def diminuir(preco=0, taxa=0):
+    res = preco - (preco * taxa / 100)
+    return moeda(res)
+
+
+def dobro(preco):
+    res = preco * 2
+    return moeda(res)
+
+
+def metade(preco):
+    res = preco / 2
+    return moeda(res)
+
+
+def moeda(preco=0, moeda='R$'):
+    return f'{moeda}{preco:.2f}'.replace('.', ',')
