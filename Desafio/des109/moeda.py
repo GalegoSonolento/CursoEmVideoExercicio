a78926f4a7a5d95deae347d22e45fc6f7b10c46c
HenryH <henriquehaag11@gmail.com>
2022-10-22 16:07:59 -0300

    Exercicio109

--- Desafio/des109/moeda.py
+++ Desafio/des109/moeda.py
@@ -1,27 +1,3 @@
-# def aumentar(preco=0, taxa=0):
-#     res = preco + (preco * taxa / 100)
-#     return moeda(res)
-#
-#
-# def diminuir(preco=0, taxa=0):
-#     res = preco - (preco * taxa / 100)
-#     return moeda(res)
-#
-#
-# def dobro(preco):
-#     res = preco * 2
-#     return moeda(res)
-#
-#
-# def metade(preco):
-#     res = preco / 2
-#     return moeda(res)
-#
-#
-# def moeda(preco=0, moeda='R$'):
-#     return f'{moeda}{preco:.2f}'.replace('.', ',')
-#
-
 def aumentar(n, taxa, format=False):
     res = n + (n * taxa / 100)
     if format:
