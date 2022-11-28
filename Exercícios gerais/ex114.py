e3430afcaa18ff2a970c6ff235d2417def7eca78
HenryH <henriquehaag11@gmail.com>
2022-11-28 17:49:01 -0300

    Des115a

--- Exercícios gerais/ex114.py
+++ Exercícios gerais/ex114.py
@@ -1,9 +1,10 @@
 import urllib
 import urllib.request
 
-try :
-  site = urllib.request.urlopen('http://www.pudim.com.br')
-except:
-  print('Deu erro')
+try:
+    site = urllib.request.urlopen('http://www.pudim.com.br')
+except urllib.error.URLError:
+    print('O site pudim não está acessível no momento')
 else:
-  print('Tudo certo')
+    print('Consegui acessar o site pudim com sucesso')
+    # print(site.read())
