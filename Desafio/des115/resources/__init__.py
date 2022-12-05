95aaf7625bfa303d1a6cb77175bd413a9bf4bf09
HenryH <henriquehaag11@gmail.com>
2022-12-05 07:51:25 -0300

    Des115b

--- Desafio/des115/resources/__init__.py
+++ Desafio/des115/resources/__init__.py
@@ -1,2 +1,9 @@
 # Exercício Python 115b: Vamos ver como fazer acesso a arquivos usando o Python.
+# preciso fazer uma classe de acesso que vai identificar se já existe um arquivo, vai lê-lo e escrever nele
+# Dentro da util talvez?
+
+arquivo = open('./resources/files/teste2.txt', 'w')
+arquivo.write('Cai de cabeça')
+arquivo.write('Kinda sus')
+arquivo.close()
 
